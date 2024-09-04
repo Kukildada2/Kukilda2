d434dc8356e5a296f752bb039645f4d45b7d204b
Kukildada2 <leemacc899+2@gmail.com>
2024-09-04 13:13:32 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -7,10 +7,10 @@ import certifi
 from telebot.types import ReplyKeyboardMarkup, KeyboardButton
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-TOKEN = '7500366284:AAG8DF9FOZUyi1DCagFg4b0ypIDaJaToBTM'
+TOKEN = '6893409715:AAGvnZJF5HqljhdGcaNQGaK1F6qxeOGf_10'
 MONGO_URI = 'mongodb+srv://admin:kpR4ObsewTySq48I@test.zeqrmgb.mongodb.net/test_db?retryWrites=true&w=majority'
-CHANNEL_ID = -1002150195409
-ADMIN_IDS = [1051815609, 1234567890, 2345678901, 3456789012, 4567890123]
+CHANNEL_ID = -1002193873450
+ADMIN_IDS = [1261956873]
 
 
 client = MongoClient(MONGO_URI, tlsCAFile=certifi.where())
